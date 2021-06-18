0a81ba686485e606fec82161272bd7b1c3e1d16d
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2021-06-18 20:33:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import NewExpense from './components/NewExpense/NewExpense'
-import ExpenseItem from './components/Expenses/ExpenseItem'
+import Expense from './components/Expenses/Expense'
 
 
 function App() {
@@ -13,32 +13,14 @@ function App() {
     {title: "Fees", date: new Date(2021,4,5), amount:16455},
   ]
   
+  const addExpenseHandler = (expense)=>{
+     console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense />
-       <ExpenseItem
-         title={expenses[0].title}
-         date={expenses[0].date}
-         amount={expenses[0].amount}
-       />
-
-       <ExpenseItem
-         title={expenses[1].title}
-         date={expenses[1].date}
-         amount={expenses[1].amount}
-       />
-
-       <ExpenseItem
-         title={expenses[2].title}
-         date={expenses[2].date}
-         amount={expenses[2].amount}
-       />
-       
-        <ExpenseItem
-         title={expenses[3].title}
-         date={expenses[3].date}
-         amount={expenses[3].amount}
-       />
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expense item={expenses} />
     </div>
   );
 }
